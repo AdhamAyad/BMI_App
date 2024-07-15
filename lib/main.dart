42050e8b3152d5e8bfd4aa5e8aaf725144ecfb36
Adham Ayad <aboayad557@gmail.com>
2024-07-15 19:48:31 +0200

    UI Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_app/result.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,26 +6,38 @@ void main() {
 }
 
 class BMIApp extends StatelessWidget {
-  const BMIApp({super.key});
+const BMIApp({super.key});   
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false, //? to remove debug banner
-      title: 'Flutter Demo',
+      //title: 'Flutter Demo',
       
-      home: HomePAge(),
+      home: HomePage(),
     );
   }
 }
 
-class HomePAge extends StatelessWidget {
-  const HomePAge({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int hight = 150;
+
+  int age = 15;
+
+  double wight = 50;
+
+  bool isMale = true;
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
 
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -39,45 +52,59 @@ class HomePAge extends StatelessWidget {
         children: [
           Row(children: [
             Expanded(
-              child: Container(
-                margin: const EdgeInsets.all(16),
-              height: MediaQuery.sizeOf(context).height * .22,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.blue
+              child: GestureDetector(
+                onTap: () => setState(() {
+                  isMale = false;
+                }),
+                child: Container(
+                  margin: const EdgeInsets.all(16),
+                height: MediaQuery.sizeOf(context).height * .22,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.red,
+                  width: isMale? 0:4),
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.blue
+                ),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                  Icon(Icons.female_outlined, size: 50,color: Colors.white,),
+                  Text('Female')
+                ],),
+                          ),
               ),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                Icon(Icons.female_outlined, size: 50,color: Colors.white,),
-                Text('Female')
-              ],),
-                        ),
             ),
 
 
 
           Expanded(
-            child:  Container(
-              margin: const EdgeInsets.all(16),
-              height: MediaQuery.sizeOf(context).height * .22, //? make it responsive
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.blue
+            child: InkWell(
+              onTap: () => setState(() { //? set state will change the border color whe the bool changes
+                isMale = true;
+              }),
+              child: Container(
+                margin: const EdgeInsets.all(16),
+                height: MediaQuery.sizeOf(context).height * .22, //? make it responsive
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.red,
+                  width: isMale? 4:0),
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.blue
+                ),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                  Icon(Icons.male_outlined, size: 50,color: Colors.white,),
+                  Text('Male')
+                  
+                ],),
               ),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                Icon(Icons.male_outlined, size: 50,color: Colors.white,),
-                Text('Male')
-                
-              ],),
             ),
           )
           ],),
 
           Container(
-            height: MediaQuery.sizeOf(context).height *.22,
+            height: MediaQuery.sizeOf(context).height *.22, //? make app responsable
             margin: const EdgeInsets.symmetric(horizontal: 16),
             decoration: BoxDecoration(
               color: Colors.blue,
@@ -89,13 +116,17 @@ class HomePAge extends StatelessWidget {
 
                 const Text('hight'),
 
-                const Text('150'),
+                Text('$hight'),
 
                 Slider(
-                  value: 150, //? initial value
+                  value: hight.toDouble(), //? initial value
                   min: 0,
-                  max: 200,
-                 onChanged: (value){}),
+                  max: 250,
+                onChanged: (value){
+                  setState(() {
+                    hight = value.toInt();
+                  });
+                }),
               ],
             ),
           ),
@@ -108,20 +139,33 @@ class HomePAge extends StatelessWidget {
               margin: const EdgeInsets.all(16),
               decoration: BoxDecoration(color: Colors.blue,
               borderRadius: BorderRadius.circular(16)),
-              child: Column(children: [
-                const Text('hight'),
-                const Text('150'),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, //? evenly → space is equal
+                children: [
+
+                const Text('Age'),
+                Text('$age'),
+                
+
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                   Container(
                     decoration: BoxDecoration(color: Colors.blueGrey,
                     borderRadius: BorderRadius.circular(8)),
-                    child: IconButton(onPressed: (){}, icon: const Icon(Icons.remove))),
+                    child: IconButton(onPressed: (){
+                      setState(() {
+                        age--;
+                      });
+                    }, icon: const Icon(Icons.remove))),
                     Container(
                     decoration: BoxDecoration(color: Colors.blueGrey,
                     borderRadius: BorderRadius.circular(8)),
-                    child: IconButton(onPressed: (){}, icon: const Icon(Icons.add)))
+                    child: IconButton(onPressed: (){
+                      setState(() {
+                        age++;
+                      });
+                    }, icon: const Icon(Icons.add)))
                 ],)
               ],),
             ),
@@ -132,27 +176,41 @@ class HomePAge extends StatelessWidget {
               height: 200,
               decoration:  BoxDecoration(color: Colors.blue,
               borderRadius: BorderRadius.circular(16)),
-              child: Column(children: [
-                const Text('hight'),
-                const Text('150'),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                const Text('Wight'),
+                Text('$wight'),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                   Container(
                     decoration: BoxDecoration(color: Colors.blueGrey,
                     borderRadius: BorderRadius.circular(8)),
-                    child: IconButton(onPressed: (){}, icon: const Icon(Icons.remove))),
+                    child: IconButton(onPressed: (){
+                      setState(() {
+                        wight--;
+                      });
+                    }, icon: const Icon(Icons.remove))),
                     Container(
                     decoration: BoxDecoration(color: Colors.blueGrey,
                     borderRadius: BorderRadius.circular(8)),
-                    child: IconButton(onPressed: (){}, icon: const Icon(Icons.add)))
+                    child: IconButton(onPressed: (){
+                      setState(() {
+                        wight+=5;
+                      });
+                    }, icon: const Icon(Icons.add)))
                 ],)
               ],),
             ))
           ],),
 
-          Row(children: [
-            Expanded(child: ElevatedButton(onPressed: (){},
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            Expanded(child: ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const ResultPage(),));
+            },
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.blue, //? color of button
               foregroundColor: Colors.white //? color of child and color of effect when press
